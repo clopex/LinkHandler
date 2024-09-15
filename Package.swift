052b8193abe16d21fe2b235b8104160ef1466b58
Adis Mulabdic <adis.mulabdic@gmail.com>
2024-09-15 18:35:22 +0200

    Update version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LinkHandler",
     platforms: [
-           .iOS(.v12),
+           .iOS(.v14),
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
